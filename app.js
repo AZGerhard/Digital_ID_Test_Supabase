1358beec25ac7f300b5c60243ba4cc211f1f960c
gfriess <gerhard.friess@az-armaturen.de>
2025-12-08 13:43:26 +0100

    des

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/dist/esm/supabase.js";
-
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 
 // Supabase
